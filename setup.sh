7749817203dcfd86b733c629291bbad171265958
Ubuntu <ubuntu@ip-10-112-71-116.ec2.internal>
2013-07-06 06:31:19 +0000

    init

--- setup.sh
+++ setup.sh
@@ -23,3 +23,6 @@ sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs 
+
+npm install cheerio
+npm install commander
